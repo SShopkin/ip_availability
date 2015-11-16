73055154f53af35f6d9a9dd03e0ea9d062b710fc
SShopkin <monimont2008@abv.bg>
2015-11-16 17:53:56 +0200

    Update commands, update user, update server

--- socketexam/src/socketexam/User.java
+++ socketexam/src/socketexam/User.java
@@ -1,5 +1,25 @@
 package socketexam;
 
-public class User {
+import java.net.Socket;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
+public class User {
+	private String name;
+	private boolean isItLogin;
+	private int howTimesIsLogin;
+	private Interval whenIsIn;
+	private Socket socket;
+	private List<Interval> invited = new LinkedList<Interval>();
+	
+	public User(String name, Socket socket){
+		this.name=name;
+		this.socket=socket;
+		isItLogin = true;
+		howTimesIsLogin = 1;
+		whenIsIn = new Interval();
+		whenIsIn.setFrom(new Date(System.currentTimeMillis()));
+	}
+	
 }
